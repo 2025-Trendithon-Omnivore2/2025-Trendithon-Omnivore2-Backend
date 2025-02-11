80c532af327e267be9b356dbc3d4a2d4f398d581
codemaker-kim <tnals72441@daum.net>
2025-02-12 01:03:30 +0900

    Feat(#4) - 케이크 엔티티 관련 파일 생성

---
+++ src/main/java/com/example/test/omnivore2trendithon2025/cake/api/CakeApiController.java
@@ -0,0 +1,11 @@
+package com.example.test.omnivore2trendithon2025.cake.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cakes")
+public class CakeApiController {
+}
---
+++ src/main/java/com/example/test/omnivore2trendithon2025/cake/domain/Cake.java
@@ -0,0 +1,18 @@
+package com.example.test.omnivore2trendithon2025.cake.domain;
+
+import com.example.test.omnivore2trendithon2025.global.entity.BaseEntity;
+import com.example.test.omnivore2trendithon2025.global.entity.Status;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Cake extends BaseEntity {
+
+}
