ed63aeb708a6b25c564ec16de4cabddec8ed6baa
codemaker-kim <tnals72441@daum.net>
2025-02-17 02:57:53 +0900

    Fix(#4): S3 키 인식 에러 이슈 해결

--- src/main/java/com/example/test/omnivore2trendithon2025/cake/domain/cakecandle/application/Image/S3Service.java
+++ src/main/java/com/example/test/omnivore2trendithon2025/cake/domain/cakecandle/application/Image/S3Service.java
@@ -1,56 +1,43 @@
 package com.example.test.omnivore2trendithon2025.cake.domain.cakecandle.application.Image;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.test.omnivore2trendithon2025.cake.domain.cakecandle.exception.ImageUploadFailException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class S3Service {
 
-    private final S3Client s3Client;
+    private final AmazonS3 s3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
     private final String DIR_PATH = "/images";
 
-    public String uploadImg(MultipartFile img, Long memberId) throws S3Exception { // 나중에 사용자 구분자 email로 변경하기
+    public String uploadImg(MultipartFile img, Long memberId) throws IOException {
 
-        String fileKey = DIR_PATH + "/" + memberId.toString() + img.getOriginalFilename();
+        String fileKey = DIR_PATH + "/" + memberId.toString() + "/" + img.getOriginalFilename();
 
-        try{
-            InputStream inputStream = img.getInputStream();
+        InputStream inputStream = img.getInputStream();
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(img.getSize());
+        metadata.setContentType(img.getContentType());
 
-            PutObjectRequest objectRequest = PutObjectRequest.builder()
-                    .bucket(bucket)
-                    .key(fileKey)
-                    .contentType(img.getContentType())
-                    .build();
+        PutObjectRequest objectRequest = new PutObjectRequest(bucket, fileKey,inputStream, metadata);
 
-            s3Client.putObject(objectRequest, RequestBody.fromInputStream(inputStream, img.getSize()));
+        s3Client.putObject(objectRequest);
 
-            return s3Client.utilities().getUrl(builder -> builder.bucket(bucket)
-                    .key(img.getOriginalFilename())).toString();
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-
-        throw new ImageUploadFailException();
+        return s3Client.getUrl(bucket, fileKey).toString();
     }
 }
--- src/main/java/com/example/test/omnivore2trendithon2025/global/config/S3Config.java
+++ src/main/java/com/example/test/omnivore2trendithon2025/global/config/S3Config.java
@@ -1,17 +1,17 @@
 package com.example.test.omnivore2trendithon2025.global.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
     @Value("${cloud.aws.credentials.secret-key}")
@@ -21,13 +21,12 @@ public class S3Config {
     private String region;
 
     @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey))
-                )
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(creds))
                 .build();
     }
 }
