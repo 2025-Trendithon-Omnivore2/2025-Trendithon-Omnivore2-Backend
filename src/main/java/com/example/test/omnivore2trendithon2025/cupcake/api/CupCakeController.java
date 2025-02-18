efc1579cb59e6daf0c07e9eae0e9dd6a0704f98b
codemaker-kim <tnals72441@daum.net>
2025-02-19 06:56:12 +0900

    Feat(#21): 컵케이크 스웨거 operation 작업

--- src/main/java/com/example/test/omnivore2trendithon2025/cupcake/api/CupCakeController.java
+++ src/main/java/com/example/test/omnivore2trendithon2025/cupcake/api/CupCakeController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/cupcakes")
-public class CupCakeController {
+public class CupCakeController implements CupCakeDocs{
 
     private final CupCakeService cupCakeService;
 
