27d879e0aed945bff39ec79710a6b0fc80f2d892
inhooo00 <chldlsgh0987@naver.com>
2025-02-18 20:22:05 +0900

    Feat(#17): cupCake 좋아요 docs 추가

--- src/main/java/com/example/test/omnivore2trendithon2025/heart/api/HeartDocs.java
+++ src/main/java/com/example/test/omnivore2trendithon2025/heart/api/HeartDocs.java
@@ -1,6 +1,7 @@
 package com.example.test.omnivore2trendithon2025.heart.api;
 
 import com.example.test.omnivore2trendithon2025.cake.api.dto.response.SurveyResponse;
+import com.example.test.omnivore2trendithon2025.cupcake.api.dto.response.SaveCupCakeResponse;
 import com.example.test.omnivore2trendithon2025.global.annotation.CurrentUserEmail;
 import com.example.test.omnivore2trendithon2025.global.template.RspTemplate;
 import com.example.test.omnivore2trendithon2025.member.follow.api.dto.request.FollowReqDto;
@@ -17,9 +18,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Tag(name = "[좋아요 API]", description = "좋아요 API")
 public interface HeartDocs {
 
-    @Operation(summary = "호출했을 때 좋아요 등록/삭제", description = "좋아요가 안 되어있으면 등록, 좋아요가 되어있으면 삭제 진행.",
+    @Operation(summary = "호출했을 때 케이크 좋아요 등록/삭제", description = "좋아요가 안 되어있으면 등록, 좋아요가 되어있으면 삭제 진행.",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "좋아요 등록/삭제 성공"),
+                    @ApiResponse(responseCode = "200", description = "케이크 좋아요 등록/삭제 성공"),
                     @ApiResponse(responseCode = "400", description = "잘못된 요청"),
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
                     @ApiResponse(responseCode = "500", description = "서버 오류")
@@ -27,4 +28,15 @@ public interface HeartDocs {
     RspTemplate<SurveyResponse> createOrDeleteCakeHeart(
             @Parameter(description = "로그인한 유저의 이메일(토큰에서 자동 추출)", hidden = true) String email,
             @Parameter(description = "cake 고유 Id", required = true) Long cakeId);
+
+    @Operation(summary = "호출했을 때 컵케이크 좋아요 등록/삭제", description = "좋아요가 안 되어있으면 등록, 좋아요가 되어있으면 삭제 진행.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "컵케이크 좋아요 등록/삭제 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "500", description = "서버 오류")
+            })
+    RspTemplate<SaveCupCakeResponse> createOrDeleteCupCakeHeart(
+            @Parameter(description = "로그인한 유저의 이메일(토큰에서 자동 추출)", hidden = true) String email,
+            @Parameter(description = "cup-cake 고유 Id", required = true) Long cupCakeId);
 }
