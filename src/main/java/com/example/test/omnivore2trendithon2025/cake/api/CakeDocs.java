952f68a5d7382f5ee47c32c2727f10cc13ad5b5c
codemaker-kim <tnals72441@daum.net>
2025-02-19 16:59:46 +0900

    Feat(#21): 팔로워 케이크 조회 api 스웨거 문서 작성

--- src/main/java/com/example/test/omnivore2trendithon2025/cake/api/CakeDocs.java
+++ src/main/java/com/example/test/omnivore2trendithon2025/cake/api/CakeDocs.java
@@ -4,6 +4,7 @@ import com.example.test.omnivore2trendithon2025.cake.api.dto.request.SurveyReque
 import com.example.test.omnivore2trendithon2025.cake.api.dto.response.MyCakeResponse;
 import com.example.test.omnivore2trendithon2025.cake.api.dto.response.OtherCakeResponse;
 import com.example.test.omnivore2trendithon2025.cake.api.dto.response.SurveyResponse;
+import com.example.test.omnivore2trendithon2025.global.annotation.CurrentUserEmail;
 import com.example.test.omnivore2trendithon2025.global.template.RspTemplate;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -11,6 +12,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Tag(name = "[케이크 API]", description = "케이크 관련 API")
 public interface CakeDocs {
@@ -63,4 +67,18 @@ public interface CakeDocs {
             })
     RspTemplate<MyCakeResponse> findByMemberId(
             @Parameter(description = "사용자 고유 ID", required = true) Long memberId);
+
+
+    @Operation(summary = "팔로워 케이크 조회", description = "사용자의 팔로워 케이크 목록을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "케이크 조회 성공",
+                            content = @Content(schema = @Schema(implementation = OtherCakeResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "500", description = "서버 오류")
+            })
+    RspTemplate<List<OtherCakeResponse>> findFollowerCakes(
+            @Parameter(description = "로그인한 유저의 이메일(토큰에서 자동 추출)") String email,
+            @Parameter(description = "페이지 번호", required = true) int page,
+            @Parameter(description = "요청할 개수", required = true) int size);
 }
