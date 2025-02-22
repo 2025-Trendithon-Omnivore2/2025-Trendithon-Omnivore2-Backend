53eb16a6a4b1cb8b5e843ce36d1d4700316c7e32
codemaker-kim <tnals72441@daum.net>
2025-02-22 21:10:24 +0900

    Feat(#30): 팔로워 컵케이크 조회 docs 작성

--- src/main/java/com/example/test/omnivore2trendithon2025/cupcake/api/CupCakeDocs.java
+++ src/main/java/com/example/test/omnivore2trendithon2025/cupcake/api/CupCakeDocs.java
@@ -4,6 +4,7 @@ import com.example.test.omnivore2trendithon2025.cupcake.api.dto.request.CupCakeY
 import com.example.test.omnivore2trendithon2025.cupcake.api.dto.request.CupCakeYearMonthWithAccessRequest;
 import com.example.test.omnivore2trendithon2025.cupcake.api.dto.request.SaveCupCakeRequest;
 import com.example.test.omnivore2trendithon2025.cupcake.api.dto.response.CupCakeResponse;
+import com.example.test.omnivore2trendithon2025.cupcake.api.dto.response.FollowCupCakeResponse;
 import com.example.test.omnivore2trendithon2025.cupcake.api.dto.response.SaveCupCakeResponse;
 import com.example.test.omnivore2trendithon2025.global.template.RspTemplate;
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,7 +33,7 @@ public interface CupCakeDocs {
 
     @Operation(summary = "컵케이크 조회", description = "컵케이크 고유 id로 컵케이크를 조회합니다.",
             responses = {
-                    @ApiResponse(responseCode = "201", description = "컵케이크 조회 성공",
+                    @ApiResponse(responseCode = "200", description = "컵케이크 조회 성공",
                             content = @Content(schema = @Schema(implementation = CupCakeResponse.class))),
                     @ApiResponse(responseCode = "400", description = "잘못된 요청"),
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
@@ -45,7 +46,7 @@ public interface CupCakeDocs {
 
     @Operation(summary = "내 컵케이크 목록 조회", description = "특정한 년도와 달 기준으로 존재하는 컵케이크들을 조회합니다.",
             responses = {
-                    @ApiResponse(responseCode = "201", description = "컵케이크 조회 성공",
+                    @ApiResponse(responseCode = "200", description = "컵케이크 조회 성공",
                             content = @Content(schema = @Schema(implementation = CupCakeResponse.class))),
                     @ApiResponse(responseCode = "400", description = "잘못된 요청"),
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
@@ -57,7 +58,7 @@ public interface CupCakeDocs {
 
     @Operation(summary = "내 컵케이크 접근 권한 필터 목록 조회", description = "특정한 년도와 달, 접근 권한 기준으로 존재하는 컵케이크들을 조회합니다.",
             responses = {
-                    @ApiResponse(responseCode = "201", description = "컵케이크 조회 성공",
+                    @ApiResponse(responseCode = "200", description = "컵케이크 조회 성공",
                             content = @Content(schema = @Schema(implementation = CupCakeResponse.class))),
                     @ApiResponse(responseCode = "400", description = "잘못된 요청"),
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
@@ -66,4 +67,19 @@ public interface CupCakeDocs {
     RspTemplate<List<CupCakeResponse>> findMyCupCakesWithAccessRange(
             @Parameter(description = "로그인한 유저의 이메일(토큰에서 자동 추출)", hidden = true) String email,
             @Parameter(description = "조회하려는 년도와 달(yyyy-MM)과 접근 권한", required = true)CupCakeYearMonthWithAccessRequest request);
+
+
+    @Operation(summary = "내 팔로워 컵케이크 목록 조회", description = "내 팔로워의 컵케이크를 최신순으로 조회합니다.",
+        responses = {
+                @ApiResponse(responseCode = "200", description = "컵케이크 조회 성공",
+                        content = @Content(schema = @Schema(implementation = CupCakeResponse.class))),
+                @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                @ApiResponse(responseCode = "401", description = "인증 실패"),
+                @ApiResponse(responseCode = "500", description = "서버 오류")
+        })
+    RspTemplate<List<FollowCupCakeResponse>> findFollowerCupCakes(
+            @Parameter(description = "로그인한 유저의 이메일(토큰에서 자동 추출)", hidden = true) String email,
+            @Parameter(description = "페이지 번호", required = true) int page,
+            @Parameter(description = "요청할 개수", required = true) int size);
 }
+
